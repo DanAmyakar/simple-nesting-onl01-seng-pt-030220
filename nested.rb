c26998c71e277094cda3aa1e0fae7b55fd48f7cf
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-15 20:22:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,12 @@ def adding_matz
         }
      }
   
-  programmer_hash[:yukihiro_matsumoto] = :known_for, :languages
-  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+  programmer_hash << {:yukihiro_matsumoto =>
+                        {
+                          :known_for = "Ruby",
+                          :languages = ["LISP", "C"]
+                        }
+                      }
   
   programmer_hash
     
